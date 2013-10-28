7b4a822b2af9b4fef1a7954dc7619a711827273d
almendra <almendraa@holi.cl>
2013-10-28 18:57:45 -0300

    Primeros avances

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'a30ce034bfdd0ea86dab5968334e3d402dd3832b28d1abebdc86b16438a2dc79a622e9d9731dae607604a10f7121e4eaf40b9db40c14ad055e72d261ad2e610f'
---
+++ config/routes.rb
@@ -0,0 +1,47 @@
+Blog::Application.routes.draw do  
+    resources :posts
+    root 'welcome#index'
+  #       get 'recent', on: :collection
+  #       get 'short'
+  #       get 'sold'
+  #       post 'toggle'
+  #     # (app/controllers/admin/products_controller.rb)
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     collection do
+  #     end
+  #     end
+  #     end
+  #     member do
+  #     post 'toggle'
+  #     resource :seller
+  #     resources :comments
+  #     resources :comments, :sales
+  #     resources :products
+  #     resources :sales do
+  #   concern :toggleable do
+  #   end
+  #   end
+  #   end
+  #   end
+  #   end
+  #   get 'products/:id' => 'catalog#view'
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  #   namespace :admin do
+  #   resources :photos, concerns: :toggleable
+  #   resources :posts, concerns: :toggleable
+  #   resources :products
+  #   resources :products do
+  #   resources :products do
+  #   resources :products do
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  # Example of regular route:
+  # Example resource route (maps HTTP verbs to controller actions automatically):
+  # Example resource route with concerns:
+  # Example resource route with more complex sub-resources:
+  # Example resource route with options:
+  # Example resource route with sub-resources:
+  # Example resource route within a namespace:
+  # See how all your routes lay out with "rake routes".
+  # The priority is based upon order of creation: first created -> highest priority.
+  # You can have the root of your site routed with "root"
+end
