a7312c8dfbdbb115981e145904d93f736a3c7835
almendra <almendraa@holi.cl>
2013-11-04 19:05:32 -0300

    Avances bonitos

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,11 +17,28 @@ class PostsController < ApplicationController
    	def show
   		@post = Post.find(params[:id])
 	end
-	
+
 	def edit
   		@post = Post.find(params[:id])
 	end
 
+	def update
+  		@post = Post.find(params[:id])
+ 
+  		if @post.update(params[:post].permit(:title, :text))
+    		redirect_to @post
+  		else
+    		render 'edit'
+  		end
+	end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+ 
+  redirect_to posts_path
+end
+	
 	private
   		def post_params
     		params.require(:post).permit(:title, :text)
